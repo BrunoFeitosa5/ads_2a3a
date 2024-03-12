3afb440671070dc767e72724be4deced843206be
Professor Bruno <bruno.cultura205@outlook.com>
2024-03-12 10:59:01 -0300

    final da aula

---
+++ aula05_javascript_parte2/exemplo01.js
@@ -0,0 +1,6 @@
+var nome = "Luiz"
+if (nome == "Luiz"){
+    //let idade = 40
+}
+
+console.log(idade)
---
+++ aula05_javascript_parte2/exemplo02.js
@@ -0,0 +1,10 @@
+/*
+var nome = "Luiz"
+var nome = 30
+console.log(nome)
+*/
+
+let nome = "Luiz"
+//let nome ="joao"
+nome = "João"
+console.log(nome)
